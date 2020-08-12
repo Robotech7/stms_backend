f3fcc48bb08df1b639a7754dd4dcfb778f9b42ee
Robotech7 <mironov.pavel2010@gmail.com>
2020-08-12 16:51:35 +1000

    Создание и обновление полей переведено на функционал WritableNestedModelSerializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,3 +1,4 @@
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 from rest_framework import serializers
 
 from products.models import Products
@@ -20,37 +21,37 @@ class ProductsInOrderSerializers(serializers.ModelSerializer):
         read_only_fields = ('price_for_one', 'order')
 
 
-class OrdersSerializer(serializers.ModelSerializer):
+class OrdersSerializer(WritableNestedModelSerializer):
     productsinorder_set = ProductsInOrderSerializers(many=True)
     user = serializers.SlugRelatedField(slug_field='username', read_only=True)
 
-    def create(self, validated_data):
-        products_in_order = validated_data.pop('productsinorder_set')
-        order = Orders.objects.create(**validated_data)
-        for product in products_in_order:
-            ProductsInOrder.objects.create(**product, order=order)
-        return order
-
-    def update(self, instance, validated_data):
-        instance.client_phone = validated_data.get('client_phone', instance.client_phone)
-        instance.client_name = validated_data.get('client_name', instance.client_name)
-        instance.client_surname = validated_data.get('client_surname', instance.client_surname)
-        instance.client_email = validated_data.get('client_email', instance.client_email)
-        instance.status = validated_data.get('status', instance.status)
-        instance.save()
-
-        products = validated_data.get('productsinorder_set')
-        if products is not None:
-            for product in products:
-                product_id = product.get('id', None)
-                if product_id:
-                    product_item = ProductsInOrder.objects.get(id=product_id, order=instance)
-                    product_item.amount = product.get('amount', product_item.amount)
-                    product_item.save()
-                else:
-                    new_product = ProductsInOrder(order=instance, **product)
-                    new_product.save()
-        return instance
+    # def create(self, validated_data):
+    #     products_in_order = validated_data.pop('productsinorder_set')
+    #     order = Orders.objects.create(**validated_data)
+    #     for product in products_in_order:
+    #         ProductsInOrder.objects.create(**product, order=order)
+    #     return order
+    #
+    # def update(self, instance, validated_data):
+    #     instance.client_phone = validated_data.get('client_phone', instance.client_phone)
+    #     instance.client_name = validated_data.get('client_name', instance.client_name)
+    #     instance.client_surname = validated_data.get('client_surname', instance.client_surname)
+    #     instance.client_email = validated_data.get('client_email', instance.client_email)
+    #     instance.status = validated_data.get('status', instance.status)
+    #     instance.save()
+    #
+    #     products = validated_data.get('productsinorder_set')
+    #     if products is not None:
+    #         for product in products:
+    #             product_id = product.get('id', None)
+    #             if product_id:
+    #                 product_item = ProductsInOrder.objects.get(id=product_id, order=instance)
+    #                 product_item.amount = product.get('amount', product_item.amount)
+    #                 product_item.save()
+    #             else:
+    #                 new_product = ProductsInOrder(order=instance, **product)
+    #                 new_product.save()
+    #     return instance
 
     class Meta:
         model = Orders
--- products/serializers.py
+++ products/serializers.py
@@ -1,3 +1,4 @@
+from drf_writable_nested import WritableNestedModelSerializer
 from rest_framework import serializers
 
 from .models import Products, Categories, ProductsImage
@@ -11,36 +12,36 @@ class ProductsImageSerializer(serializers.ModelSerializer):
         fields = ['image', 'id']
 
 
-class ProductsSerializer(serializers.ModelSerializer):
+class ProductsSerializer(WritableNestedModelSerializer):
     productsimage_set = ProductsImageSerializer(many=True)
     category = serializers.SlugRelatedField(slug_field='name', queryset=Categories.objects.all())
 
-    def create(self, validated_data):
-        products_image = validated_data.pop('productsimage_set')
-        product = Products.objects.create(**validated_data)
-        for image in products_image:
-            ProductsImage.objects.create(product=product, **image)
-        return product
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.category = validated_data.get('category', instance.category)
-        instance.is_active = validated_data.get('is_active', instance.is_active)
-        instance.description = validated_data.get('description', instance.description)
-        instance.price = validated_data.get('price', instance.price)
-        instance.in_storage = validated_data.get('in_storage', instance.in_storage)
-        instance.bar_code = validated_data.get('bar_code', instance.bar_code)
-        instance.save()
-        product_images = validated_data.pop('productsimage_set')
-        for image in product_images:
-            image_id = image.get('id', None)
-            if image_id:
-                product_image = ProductsImage.objects.get(id=image_id)
-                product_image.image = image.get('image', product_image.image)
-                product_image.save()
-            else:
-                ProductsImage.objects.create(product=instance, **image)
-        return instance
+    # def create(self, validated_data):
+    #     products_image = validated_data.pop('productsimage_set')
+    #     product = Products.objects.create(**validated_data)
+    #     for image in products_image:
+    #         ProductsImage.objects.create(product=product, **image)
+    #     return product
+    #
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.category = validated_data.get('category', instance.category)
+    #     instance.is_active = validated_data.get('is_active', instance.is_active)
+    #     instance.description = validated_data.get('description', instance.description)
+    #     instance.price = validated_data.get('price', instance.price)
+    #     instance.in_storage = validated_data.get('in_storage', instance.in_storage)
+    #     instance.bar_code = validated_data.get('bar_code', instance.bar_code)
+    #     instance.save()
+    #     product_images = validated_data.pop('productsimage_set')
+    #     for image in product_images:
+    #         image_id = image.get('id', None)
+    #         if image_id:
+    #             product_image = ProductsImage.objects.get(id=image_id)
+    #             product_image.image = image.get('image', product_image.image)
+    #             product_image.save()
+    #         else:
+    #             ProductsImage.objects.create(product=instance, **image)
+    #     return instance
 
     class Meta:
         model = Products
